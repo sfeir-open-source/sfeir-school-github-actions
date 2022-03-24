e905724e97d7ec9ec40550d7e37eea968b6ba3d7
Gaetan BOGAERT <gbogaert62@gmail.com>
2022-03-24 14:29:26 +0100

    up

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -1,7 +1,7 @@
 // One method per module
 function schoolSlides() {
   return [
-    '00-presentation/00-TITLE.md', 
+    '00-presentation/00-TITLE.md',
     '00-presentation/01-speaker-thibauld.md',
     '00-presentation/02-speaker-gaetan.md',
     '00-presentation/03-participant.md'
@@ -12,8 +12,26 @@ function introSlides() {
   return [
     '01-intro/00-TITLE.md',
     '01-intro/01-objectifs.md',
-    '01-intro/02-hors-sujet.md',
-    '01-intro/03-deroulement.md'
+    '01-intro/02-off-topic.md',
+    '01-intro/03-TOC.md'
+  ];
+}
+
+function introInteractWithEnv() {
+  return [
+    '03-Interact-With-Env/00-TITLE.md'
+  ];
+}
+
+function introReusableWorkflows() {
+  return [
+    '04-Reusable-Workflows/00-TITLE.md'
+  ];
+}
+
+function introAdvancedConceptsSlides() {
+  return [
+    '06-Advanced-Concepts/00-TITLE.md'
   ];
 }
 
@@ -21,7 +39,10 @@ function formation() {
   return [
     //
     ...schoolSlides(), //
-    ...introSlides() //
+    ...introSlides(),
+    ...introInteractWithEnv(),
+    ...introReusableWorkflows(),
+    ...introAdvancedConceptsSlides(),//
   ].map(slidePath => {
     return { path: slidePath };
   });
