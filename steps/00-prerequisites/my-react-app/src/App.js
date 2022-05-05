350b6d85cc99fe071937793aef5af225dad68810
Gaetan BOGAERT <gbogaert62@gmail.com>
2022-05-05 13:26:26 +0200

    up

--- steps/00-prerequisites/my-react-app/src/App.js
+++ steps/00-prerequisites/my-react-app/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './logo-SFEIR-blanc.png';
+
 import './App.css';
 
 function App() {
@@ -6,18 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <a
+        className="App-link"
+        href="https://sfeir-open-source.github.io/sfeir-school-github-actions"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        learn github actions
+      </a>
     </div>
   );
 }
